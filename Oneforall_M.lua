94f7a722993cdf32f405731fceeac56072944c2d
Mayhem00E <131640433+Mayhem00E@users.noreply.github.com>
2023-06-10 22:25:50 +0800

    Add files via upload

---
+++ Oneforall_M.lua
@@ -0,0 +1,97 @@
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
+
+getgenv().DebugHook =
+    "https://discord.com/api/webhooks/1101508002328105114/zFmIhMppmlQmj21EcIzs9Kv_S4evu7k4Em7k5G-Vy9SBEO-rNSrDR61kxP9n2xUFVlLN"
+getgenv().MailHook =
+    "https://discord.com/api/webhooks/1085929183487725588/phOL2IzRzkf7wwZI33Ng3bVG6JqBfuksEB0hah3VVqLRIzyNZIsz0R-1FekvevqS9SWy"
+getgenv().MilkupKey = ""
+getgenv().MailUsername = "Mayhem00E"
+getgenv().SellHook =
+    "https://discord.com/api/webhooks/1087767388902133850/EPwgwO3ERE6Tivrw2S7YTU8XX5NAFc0cw12IcMusT1vbD0Mhm6zxAjODMn5pEWmQSz6U"
+getgenv().SnipeHook =
+    "https://discord.com/api/webhooks/1087767285688709163/ZYboDxOJV1jZVR-OHsdGQ4KqASjEgs1lXnrFvHcASabc6_dzkcMgsYCgvOTT_QdeVxuN"
+getgenv().HugeKey = ""
+
+local httpService = game:GetService("HttpService")
+local promptOverlay = game.CoreGui.RobloxPromptGui.promptOverlay
+local apiUrl = "https://calculators-gross-drives-surveys.trycloudflare.com/servers"
+
+local function makeGetRequest(url)
+    local response = game:HttpGetAsync(url)
+    return httpService:JSONDecode(response)["jobID"]
+end
+
+sendError = function()
+    local errorMessage = promptOverlay.ErrorPrompt.MessageArea.ErrorFrame.ErrorMessage.Text
+    url = getgenv().DebugHook
+
+    if string.match(errorMessage, "unexpected client behavior") or string.match(errorMessage, "Please rejoin%.") then
+        data = {
+            ["content"] = "@everyone",
+            ["embeds"] = {{
+                ["title"] = game:GetService("Players").LocalPlayer.Name .. " has been disconnected!",
+                ["description"] = errorMessage,
+                ["type"] = "rich",
+                ["color"] = tonumber(0x7269da)
+            }}
+        }
+    else
+        data = {
+            ["embeds"] = {{
+                ["title"] = game:GetService("Players").LocalPlayer.Name .. " has been disconnected!",
+                ["description"] = errorMessage,
+                ["type"] = "rich",
+                ["color"] = tonumber(0x7269da)
+            }}
+        }
+    end
+
+    newdata = game:GetService("HttpService"):JSONEncode(data)
+
+    headers = {
+        ["content-type"] = "application/json"
+    }
+    request = http_request or request or HttpPost or syn.request
+    sendwebhook = {
+        Url = url,
+        Body = newdata,
+        Method = "POST",
+        Headers = headers
+    }
+
+    request(sendwebhook)
+end
+
+pcall(function()
+    if promptOverlay.ErrorPrompt.MessageArea.ErrorFrame.ErrorMessage.Text then
+        repeat
+            if errorMessage ~= "Label" then
+                sendError()
+                if string.match(errorMessage, "unexpected client behavior") or
+                    string.match(errorMessage, "Please rejoin%.") then
+                    game:Shutdown()
+                else
+                    local getResponse = makeGetRequest(apiUrl)
+                    game:GetService("TeleportService"):TeleportToPlaceInstance(7722306047, getResponse,
+                        game.Players.LocalPlayer)
+                    task.wait(300)
+                end
+            end
+            task.wait(0.5)
+        until false
+    end
+end)
+
+task.spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/miyuyumi/AutoConfig/main/Mobile/Scripts/All.lua"))()
+end)
+
+task.spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/miyuyumi/AutoConfig/main/Mobile/Scripts/Seller.lua"))()
+end)
+
+task.spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/miyuyumi/AutoConfig/main/Mobile/Scripts/Super.lua"))()
+end)
